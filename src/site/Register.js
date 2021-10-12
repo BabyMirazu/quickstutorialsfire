16eaf5c773aa6c5a59c9803dbb5bcc140329182a
BabyMirazu <iwajaki2200@gmail.com>
2021-10-12 11:08:38 +0800

    Update Register.js

--- src/site/Register.js
+++ src/site/Register.js
@@ -14,6 +14,7 @@ const Register = () => {
 
     const register = () => {
         const auth = getAuth()
+        console.log(name)
         createUserWithEmailAndPassword(auth, email.value, password.value)
         .then(() => {
             alert('success');
